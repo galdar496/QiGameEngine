71ac0945d6f7980e65b8dd6e8b5230125de18ca4
Cody White <cody.white@imgtec.com>
2015-01-10 16:15:10 -0800

    Finished implementation of vector class

--- Qi/Source/Core/Math/Vec4.h
+++ Qi/Source/Core/Math/Vec4.h
@@ -19,7 +19,7 @@ namespace Qi
 /**
   * Contains a 4 component vector and all math operations
   */
-class Vec4
+class __attribute__ ((aligned(16))) Vec4
 {
     public:
     
@@ -34,12 +34,12 @@ class Vec4
         /**
           * Constructor with per-component initialization.
           */
-        Vec4(float x, float y, float z, float w)
+        Vec4(float _x, float _y, float _z, float _w)
         {
-            m_vector.x = x;
-            m_vector.y = y;
-            m_vector.z = z;
-            m_vector.w = w;
+            x = _x;
+            y = _y;
+            z = _z;
+            w = _w;
         }
     
         /**
@@ -47,7 +47,7 @@ class Vec4
           */
         Vec4(const Vec4 &other)
         {
-            m_vector.mm_value = other.m_vector.mm_value;
+            mm_value = other.mm_value;
         }
     
         /**
@@ -55,45 +55,147 @@ class Vec4
           */
         inline void zero()
         {
-            m_vector.mm_value = _mm_setzero_ps();
+            mm_value = _mm_setzero_ps();
         }
     
         // Math Operations //////////////////////////////////////
     
-        /** 
+        /**
+          * Operator+
+          * @param other Vector to add.
+          */
+        inline Vec4 operator+(const Vec4 &other) const
+        {
+            return _mm_add_ps(mm_value, other.mm_value);
+        }
+    
+        /**
+          * Operator-
+          * @param other Vector to subtract.
+          */
+        inline Vec4 operator-(const Vec4 &other) const
+        {
+            return _mm_sub_ps(mm_value, other.mm_value);
+        }
+    
+        /**
+          * Operator*
+          * @param scalar Scalar value to multiply the vectory by.
+          */
+        inline Vec4 operator*(float scalar) const
+        {
+            return _mm_mul_ps(mm_value, _mm_set1_ps(scalar));
+        }
+    
+        /**
+         * Operator/
+         * @param scalar Scalar value to divide the vectory by.
+         */
+        inline Vec4 operator/(float scalar) const
+        {
+            return _mm_div_ps(mm_value, _mm_set1_ps(scalar));
+        }
+    
+        /**
+         * Operator*=
+         * @param scalar Scalar value to multiply the vectory by.
+         */
+        inline void operator*=(float scalar)
+        {
+            mm_value = _mm_mul_ps(mm_value, _mm_set1_ps(scalar));
+        }
+        
+        /**
+         * Operator/=
+         * @param scalar Scalar value to divide the vectory by.
+         */
+        inline void operator/=(float scalar)
+        {
+            mm_value = _mm_div_ps(mm_value, _mm_set1_ps(scalar));
+        }
+    
+        /**
           * Dot product
           * @param other Vec4 object to perform the dot product with.
           * @return Scalar value denoting the dot product.
           */
-        float dot(const Vec4 &other)
+        inline float dot(const Vec4 &other) const
         {
-            // Mask 0xF1 means to perform a dot product with all 4 components (0xF) and store it in the
-            // lowest float value of the result (0x1).
-            return _mm_cvtss_f32(_mm_dp_ps(m_vector.mm_value, other.m_vector.mm_value, 0xF1));
+            return _mm_cvtss_f32(_mm_dp_ps(mm_value, other.mm_value, SIMDMask::AllChannels_StoreLow));
         }
     
         /**
           * Cross product. Note that cross product is only defined in 3 dimensions, therefore
           * the fourth component of each vector is ignored.
           * @param other Vec4 object to perform a cross product with.
-          * @param result Resulting vector from the cross product.
+          * @return Resulting vector from the cross product.
           */
-        void cross(const Vec4 &other, Vec4 &result)
+        inline Vec4 cross(const Vec4 &other) const
         {
-            result.m_vector.mm_value = _mm_sub_ps(
-                _mm_mul_ps(_mm_shuffle_ps(m_vector.mm_value, m_vector.mm_value, _MM_SHUFFLE(3, 0, 2, 1)), _mm_shuffle_ps(other.m_vector.mm_value, other.m_vector.mm_value, _MM_SHUFFLE(3, 1, 0, 2))),
-                _mm_mul_ps(_mm_shuffle_ps(m_vector.mm_value, m_vector.mm_value, _MM_SHUFFLE(3, 1, 0, 2)), _mm_shuffle_ps(other.m_vector.mm_value, other.m_vector.mm_value, _MM_SHUFFLE(3, 0, 2, 1)))
-                                                 );
+            return _mm_sub_ps(
+                _mm_mul_ps(_mm_shuffle_ps(mm_value, mm_value, _MM_SHUFFLE(3, 0, 2, 1)), _mm_shuffle_ps(other.mm_value, other.mm_value, _MM_SHUFFLE(3, 1, 0, 2))),
+                _mm_mul_ps(_mm_shuffle_ps(mm_value, mm_value, _MM_SHUFFLE(3, 1, 0, 2)), _mm_shuffle_ps(other.mm_value, other.mm_value, _MM_SHUFFLE(3, 0, 2, 1)))
+                             );
         }
     
-    private:
+        /**
+          * Normalize the vector. The normalized version will overwrite the previous version.
+          * Note that this function is optimized for speed and may produce a normal that does not quite have a unit length (but is very close).
+          */
+        inline void normalize()
+        {
+            __m128 inverse = _mm_rsqrt_ps(_mm_dp_ps(mm_value, mm_value, SIMDMask::AllChannels_StoreAll));
+            mm_value = _mm_mul_ps(mm_value, inverse);
+        }
     
+        /**
+          * Normalize the vector accurately. This differs from standard "normalize" in that it will produce a mathematically correct
+          * normal but at much more cost.
+          */
+        inline void normalizeAccurate()
+        {
+            __m128 sqrt = _mm_sqrt_ps(_mm_dp_ps(mm_value, mm_value, SIMDMask::AllChannels_StoreAll));
+            mm_value = _mm_div_ps(mm_value, sqrt);
+        }
+    
+        /**
+          * Get the length of the vector.
+          */
+        inline float length() const
+        {
+            __m128 length_squared = _mm_dp_ps(mm_value, mm_value, SIMDMask::AllChannels_StoreLow);
+            return _mm_cvtss_f32(_mm_sqrt_ps(length_squared));
+        }
+    
+        /**
+          * Get the inverse lenght of the vector.
+          */
+        inline float lengthInverse() const
+        {
+            __m128 inv_length_squared = _mm_dp_ps(mm_value, mm_value, SIMDMask::AllChannels_StoreLow);
+            return _mm_cvtss_f32(_mm_rsqrt_ps(inv_length_squared));
+        }
+    
+        /**
+         * Construction from a SIMD type
+         */
+        Vec4(__m128 value) : mm_value(value) {}
+        
         // Definition of the vector.
         union
         {
             struct {float x, y, z, w; }; // Simple access to simd vector.
             __m128 mm_value;    // SIMD version of the vector.
-        } m_vector;  // Internal vector storage.
+        };
+    
+    private:
+    
+        // Mask values to use in all simd operations.
+        enum SIMDMask
+        {
+            AllChannels_StoreLow = 0xF1, // Use all 4 channels of the simd value during the calculation. Store the result in the lowest register of the result value.
+            AllChannels_StoreAll = 0xFF, // Use all 4 channels of the simd value during the calculation. Store the result in all registers for the result value.
+        };
 };
 
 } // Namespace Qi
