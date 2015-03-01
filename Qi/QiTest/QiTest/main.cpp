5cff2f44fc2db5a0cbb3a8c39474c8b25b008704
Cody White <cody.white@imgtec.com>
2015-03-01 11:42:50 -0800

    Initialize the logger for unit test errors.

--- Qi/QiTest/QiTest/main.cpp
+++ Qi/QiTest/QiTest/main.cpp
@@ -7,15 +7,17 @@
 //
 
 #include <gtest/gtest.h>
+#include "../../Source/Core/Utility/Logger.h"
 #include "../../Source/Core/Memory/MemoryAllocator.h"
 
 int main(int argc, const char * argv[])
 {
     ::testing::InitGoogleTest(&argc, (char **)argv);
     
-    // Make sure that the memory allocator has been initialized for
+    // Make sure that the logger and memory allocator has been initialized for
     // any tests that might use it.
-    bool ready = Qi::MemoryAllocator::getInstance().init();
+    bool ready = Qi::Logger::getInstance().init(true);
+    ready &= Qi::MemoryAllocator::getInstance().init();
     assert(ready);
     
     int result = RUN_ALL_TESTS();
